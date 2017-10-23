d02d454750c4c17a7aab2cd6b635177efbf3fe84
Dustin Roten <rotendd@appstate.edu>
2017-10-23 08:20:43 -0400

    Changing contents of inner for loop

--- Figures/NewVersion.R
+++ Figures/NewVersion.R
@@ -33,28 +33,28 @@ for (i in 1:4) {
             
         eval(parse(text = paste("Matrix_Model1 <- GridDispersions(Origin_Dispersion, ", Names_Scenarios[i], ", 1)", sep = "")))
         eval(parse(text = paste("Matrix_Model2 <- GridDispersions(Origin_Dispersion, ", Names_Scenarios[i], ", 2)", sep = "")))
-          
-    } # End individual iterations here. Matrices 1 & 2 have been filled
     
-    # Both matrices are reformatted here
-    Melted_Matrix_Model1 <- melt(Matrix_Model1)
-    Melted_Matrix_Model2 <- melt(Matrix_Model2)
-    names(Melted_Matrix_Model1) <- c("LAT", "LON", "CO2")
-    names(Melted_Matrix_Model2) <- c("LAT", "LON", "CO2")
+        # Both matrices are reformatted here
+        Melted_Matrix_Model1 <- melt(Matrix_Model1)
+        Melted_Matrix_Model2 <- melt(Matrix_Model2)
+        names(Melted_Matrix_Model1) <- c("LAT", "LON", "CO2")
+        names(Melted_Matrix_Model2) <- c("LAT", "LON", "CO2")
   
-    # Each MRS metric value is calculated here
-    MRS_Value <- (1/sum(Matrix_Model1))*sum(abs(Matrix_Model1 - Matrix_Model2))
+        # Each MRS metric value is calculated here
+        MRS_Value <- (1/sum(Matrix_Model1))*sum(abs(Matrix_Model1 - Matrix_Model2))
     
-    # The Difference in COM is calculated here
-    COM_Value <- COMMeasure(Melted_Matrix_Model1, Melted_Matrix_Model2)
+        # The Difference in COM is calculated here
+        COM_Value <- COMMeasure(Melted_Matrix_Model1, Melted_Matrix_Model2)
     
-    # Mean angle difference is calculated here
-    Angle1 <- COMAngle(Melted_Matrix_Model1)
-    Angle2 <- COMAngle(Melted_Matrix_Model2)
-    MeanAngle_Value <- if(abs(Angle1-Angle2) <= 180) {abs(Angle1 - Angle2)} else {360 - abs(Angle1 - Angle2)}
+        # Mean angle difference is calculated here
+        Angle1 <- COMAngle(Melted_Matrix_Model1)
+        Angle2 <- COMAngle(Melted_Matrix_Model2)
+        MeanAngle_Value <- if(abs(Angle1-Angle2) <= 180) {abs(Angle1 - Angle2)} else {360 - abs(Angle1 - Angle2)}
     
-    eval(parse(text = paste(Metrics[i], "[j+1,1] <- MRS_Value", sep = "")))
-    eval(parse(text = paste(Metrics[i], "[j+1,2] <- COM_Value", sep = "")))
-    eval(parse(text = paste(Metrics[i], "[j+1,3] <- MeanAngle_Value", sep = "")))
+        eval(parse(text = paste(Metrics[i], "[j+1,1] <- MRS_Value", sep = "")))
+        eval(parse(text = paste(Metrics[i], "[j+1,2] <- COM_Value", sep = "")))
+        eval(parse(text = paste(Metrics[i], "[j+1,3] <- MeanAngle_Value", sep = "")))
+    
+    } # End individual iterations here. Matrices 1 & 2 have been filled
     
 }
